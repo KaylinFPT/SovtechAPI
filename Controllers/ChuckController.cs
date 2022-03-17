2d7ca49396f8b22363a0958e86ed3df9085f9531
Kaylin <83064592+KaylinFPT@users.noreply.github.com>
2022-03-17 09:00:22 +0200

    Add project files.

---
+++ Controllers/ChuckController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SovtechAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChuckController : ControllerBase
+    {
+        HttpClient httpClient;
+        public ChuckController()
+        {
+            httpClient = new HttpClient() 
+            { 
+                BaseAddress = new Uri("https://api.chucknorris.io/")
+            };
+        }
+
+
+        [HttpGet("/chuck/categories")]
+        public async Task<string> Get()
+        {
+            var jsonCategories = await httpClient.GetStringAsync("jokes/categories");
+
+            return jsonCategories;
+        }
+
+       
+    }
+}
