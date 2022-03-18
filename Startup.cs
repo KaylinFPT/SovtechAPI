f44fb33f8dd5c11aff838053eb3480329bed5355
Kaylin <83064592+KaylinFPT@users.noreply.github.com>
2022-03-18 09:19:26 +0200

    Enabled CORS in app services to allow calls from a diffrent domain

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace SovtechAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors(); 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -37,6 +37,9 @@ namespace SovtechAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(
+                options => options.WithOrigins("https://sovtechassessment.azurewebsites.net/").AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin()
+            );
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
